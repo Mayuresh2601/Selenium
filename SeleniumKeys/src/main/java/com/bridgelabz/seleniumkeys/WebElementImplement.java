f0b4ced18e5c98c336502219c9594fa92585afad
Mayuresh2601 <mssonar26@gmail.com>
2020-01-14 12:16:27 +0530

    Mofified: WebElementImplementation Class

--- SeleniumKeys/src/main/java/com/bridgelabz/seleniumkeys/WebElementImplement.java
+++ SeleniumKeys/src/main/java/com/bridgelabz/seleniumkeys/WebElementImplement.java
@@ -11,28 +11,51 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class WebElementImplement {
 
+	static { 
+	System.setProperty("webdriver.chrome.driver", "/home/admin1/eclipse-workspace/Mayuresh/Selenium/SeleniumKeys/Driver/chromedriver");
+	System.setProperty("webdriver.gecko.driver", "/home/admin1/eclipse-workspace/Mayuresh/Selenium/SeleniumKeys/Driver/geckodriver");
+	}
+	private static WebDriver driver;
+	private static WebElement emailId;
+	private static WebElement password;
+	private static WebElement login;
+	
 	public static void main(String[] args) throws InterruptedException {
-	
-		System.setProperty("webdriver.chrome.driver", "/home/admin1/eclipse-workspace/Mayuresh/Selenium/SeleniumKeys/Driver/chromedriver");
 		
-		WebDriver driver = new ChromeDriver();
-	
+		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com");
-		WebElement emailId = driver.findElement(By.id("email"));
+		
+		emailId = driver.findElement(By.id("email"));
 		emailId.sendKeys("8109555221");
 		Thread.sleep(500);
 		
-		WebElement password = driver.findElement(By.name("pass"));
+		password = driver.findElement(By.name("pass"));
 		password.sendKeys("test@123");
 		Thread.sleep(500);
 		
-		WebElement login = driver.findElement(By.id("loginbutton"));
+		login = driver.findElement(By.id("loginbutton"));
 		login.click();
 		Thread.sleep(500);
+		driver.close();
 		
+		driver = new FirefoxDriver();
+		driver.get("https://www.facebook.com");
+		
+		emailId = driver.findElement(By.id("email"));
+		emailId.sendKeys("8109555221");
+		Thread.sleep(500);
+		
+		password = driver.findElement(By.name("pass"));
+		password.sendKeys("test@123");
+		Thread.sleep(500);
+		
+		login = driver.findElement(By.id("loginbutton"));
+		login.click();
+		Thread.sleep(500);
 		driver.close();
 	}
 
