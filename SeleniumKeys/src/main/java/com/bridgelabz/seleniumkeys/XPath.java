a7afd9df79dd30aa8f4ed3fc6ffdbd5db4468cd4
Mayuresh2601 <mssonar26@gmail.com>
2020-01-15 15:48:14 +0530

    Modified: XPath Expression

--- SeleniumKeys/src/main/java/com/bridgelabz/seleniumkeys/XPath.java
+++ SeleniumKeys/src/main/java/com/bridgelabz/seleniumkeys/XPath.java
@@ -9,10 +9,10 @@ public class XPath extends Base{
 	
 		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com");
-		
-		driver.findElement(By.xpath("//input[contains(text(),'email')]")).sendKeys("8109555221");
+
+		driver.findElement(By.xpath("//input[contains(@class,'inputtext')]")).sendKeys("8109555221");
 		Thread.sleep(1000);
-		driver.findElement(By.xpath("//input[contains(text(),'pass')]")).sendKeys("test@123");
+		driver.findElement(By.xpath("//tr/following-sibling::tr//input[@type='password']")).sendKeys("test@123");
 		Thread.sleep(1000);
 		driver.findElement(By.xpath("//input[contains(@value,'Log In')]")).click();
 		Thread.sleep(1000);
